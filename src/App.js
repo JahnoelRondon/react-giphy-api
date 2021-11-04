37aefc2cd33214bd62991811f1cb48a2a05b5eb2
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-03 21:47:19 -0400

    added giphy import component, added .env file with api key and base url, added state with baseurl using process.env however getting undefined

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 import React, {Component} from 'react'
+import GiphyInfo from './GiphyInfo';
 
 class App extends Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+      baseurl: process.env.REACT_BASE_URL
+    }
+  }
+
   render() {
     console.log(this)
     return (
---
+++ src/GiphyInfo.jsx
@@ -0,0 +1,6 @@
+
+function GiphyInfo(){
+    return <h1>giphy</h1>
+}
+
+export default GiphyInfo;
