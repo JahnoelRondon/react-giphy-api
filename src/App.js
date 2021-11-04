161de2062b2231dc9695633f0bd66eb0c09c3f08
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-04 09:15:31 -0400

    added state props for url endpoint

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ class App extends Component {
     super()
 
     this.state = {
-      baseurl: process.env.REACT_BASE_URL
+      baseurl: 'https://api.giphy.com/v1/gifs/search?',
+      api_key: 'api_key=' + '4B9b9lcflmEmNTgHwvULf4AIhhLNYARY',
+      query: '&q=',
+      giphyTitle: '',
+      searchEndPoint: ''
     }
   }
 
